960a1de6c579a274ef7c1196e1872480896eefe5
nguyenntt <106979668+Nolongeruser9771@users.noreply.github.com>
2023-09-19 15:21:20 +0700

    add lang-switch btn

--- src/layouts/nav.js
+++ src/layouts/nav.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 
 export default function Navigator() {
@@ -21,15 +22,46 @@ export default function Navigator() {
       name: "Contact",
     },
   ];
-  
+
+  //lang switch
+  const [on, setOn] = useState(
+    localStorage.getItem("ENG_ON")
+      ? JSON.parse(localStorage.getItem("ENG_ON"))
+      : true
+  );
+  const langSwitch = (targetLang) => {
+    if (targetLang) {
+      setOn(targetLang === "en");
+      localStorage.setItem("ENG_ON", JSON.stringify(targetLang === "en"));
+    }
+  };
+
   return (
     <div className="container">
       <div className="row nav">
-        <div className="col-6" />
+        <div className="col-6" style={{ textAlign: "left" }}>
+          <span
+            onClick={() => langSwitch("en")}
+            className={on ? "lang-active" : ""}
+          >
+            English
+          </span>
+          <span
+            onClick={() => langSwitch("jp")}
+            className={!on ? "lang-active" : ""}
+          >
+            日本語
+          </span>
+        </div>
         <div className="col-6">
           <div className="row navbar">
             {navbar.map((nav, index) => (
-              <div key={index} className={location===nav.pathname? "col btn active" : "col btn"}>
+              <div
+                key={index}
+                className={
+                  location === nav.pathname ? "col btn active" : "col btn"
+                }
+              >
                 <Link to={nav.pathname}>{nav.name}</Link>
               </div>
             ))}
